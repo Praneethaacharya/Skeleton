c69109d98d68a2fa97bd8dfefdf6ad7a0ae5d0f9
kpraneethaacharya@gmail.com <kpraneethaacharya@gmail.com>
2024-03-10 11:59:08 +0530

    My commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+
+    <div className="bg-gray-400 mx-5 w-465 h-fit my-5 rounded-xl ">
+      <div className='flex flex-row relative'>
+        <div className='bg-gray-200 m-5 ms-4 rounded-full h-36 w-36'></div>
+        <div className='bg-gray-200 m-2 h-8 w-60 rounded-xl mt-16'></div>
+        </div>
+        <div className='flex flex-row-reverse'>
+        <div className='bg-gray-200 h-7 w-16 rounded-xl mr-7 mt-0'></div>
+      </div>
+      <div className='bg-gray-200 m-5 rounded-xl h-48 w-465'></div>
+      <div className='bg-gray-200 m-5 rounded-xl h-10 w-465'></div>
+      <div className=' flex flex-row-reverse'>
+      <div className='bg-gray-200 mt-32 rounded-xl h-16 w-48 mr-7 mb-11'></div>
+      </div>
+   </div>
+  )
+}
+
+export default App
